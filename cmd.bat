bb3b2f14831b33121b6187bb21ee4e715f1ce9af
amrwc <42574496+amrwc@users.noreply.github.com>
2021-01-23 14:24:31 +0000

    Don't delete the image before it's used in the Batch version

--- cmd.bat
+++ cmd.bat
@@ -1,16 +1,17 @@
 :: Give your co-workers a quick information security lesson by derulo'ing them.
 :: Maybe next time they will remember to lock their machine.
-:: 
+::
 :: What?
 :: The script instantly changes the desktop's background of the machine it's run on.
-:: 
+::
 :: Why?
 :: If your workplace is friendly enough to pull off pranks on each other and
 :: some of you care about information security, the one and only Jason Derulo
 :: will be a perfect reminder that an unattended computer should be locked.
-:: 
-:: Windows-specific; amending current user's registry keys
-:: 
+::
+:: NOTE: This script is Windows-specific – it amends current user's registry
+:: keys.
+::
 :: Usage:
 :: 1. Download any '.bmp' image, call it 'derulo.bmp'. Make sure it's placed
 ::    in the same directory as the 'windows.bat' script.
@@ -18,10 +19,6 @@
 
 :: Turns off the output to the command-line
 @echo off
-
-:: Deletes `derulo.bmp` in case it's already there
-:: %CD% -- current directory (where the script is located)
-del %CD%\derulo.bmp
 
 :: Sets the wallpaper to the one supplied
 :: Source: https://social.technet.microsoft.com/Forums/en-US/w7itproui/thread/72a9b4bf-071b-47cd-877d-0c0629a9eb90/#c58b97cd-3a91-4409-80aa-39d53ef638cf
